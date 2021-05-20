a0b3d7e43a1cb3c0788b09dcfdaab67a62f7f94c
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-05-20 23:05:02 +0530

    added routes

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express= require('express');
+const articleRouter=require('./routes/articles')
 const app=express();
 
 app.set('view engine','ejs')
 
+
+app.use('/articles',articleRouter)
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    const articles=[{
+        title:'Test article',
+        createdAt:new Date(),
+        description:'Test description'
+    },{
+        title:'Test article 2',
+        createdAt:new Date(),
+        description:'Test description 235'
+    }]
+    res.render('articles/index',{articles:articles})
 })
  
 app.listen(5000);
