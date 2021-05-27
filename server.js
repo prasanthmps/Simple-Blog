7bbb172a33478f739304ba7fe452ba744989a8fc
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-05-27 23:16:45 +0530

    final commit

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express= require('express');
+const mongoose=require('mongoose');
+const methodOverride=require('method-override')
+const marked=require('marked')
+const slugify=require('slugify')
+const Article=require('./models/article')
 const articleRouter=require('./routes/articles')
 const app=express();
 
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 app.set('view engine','ejs')
 
-
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
 app.use('/articles',articleRouter)
-app.get('/',(req,res)=>{
-    const articles=[{
-        title:'Test article',
-        createdAt:new Date(),
-        description:'Test description'
-    },{
-        title:'Test article 2',
-        createdAt:new Date(),
-        description:'Test description 235'
-    }]
+
+app.get('/',async (req,res)=>{
+    const articles=await Article.find().sort({
+        createdAt:'desc'
+    })
     res.render('articles/index',{articles:articles})
 })
  
